15670661d4ec69de7fa900a3fe9f0008b9557561
George Griffiths <george.griffiths@immersivelabs.com>
2025-02-11 21:57:29 +0000

    evaulate functions and closures

--- src/monkey/object/environment.go
+++ src/monkey/object/environment.go
@@ -1,16 +1,26 @@
 package object
 
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	return env
+}
+
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
-	return &Environment{store: s}
+	return &Environment{store: s, outer: nil}
 }
 
 type Environment struct {
 	store map[string]Object
+	outer *Environment
 }
 
 func (e *Environment) Get(key string) (Object, bool) {
 	obj, ok := e.store[key]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(key)
+	}
 	return obj, ok
 }
 
