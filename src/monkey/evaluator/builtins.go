a7a84e337a122a443b9cbe6833a0f706c6882308
George Griffiths <george.griffiths@immersivelabs.com>
2025-02-15 16:26:08 +0000

    4.5 Array builtins

--- src/monkey/evaluator/builtins.go
+++ src/monkey/evaluator/builtins.go
@@ -1,8 +1,20 @@
 package evaluator
 
-import "monkey/object"
+import (
+	"fmt"
+	"monkey/object"
+)
 
 var builtins = map[string]*object.Builtin{
+	"puts": {
+		Fn: func(args ...object.Object) object.Object {
+			// TODO fix this
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+			return NULL
+		},
+	},
 	"len": {
 		Fn: func(args ...object.Object) object.Object {
 			if len(args) != 1 {
@@ -13,8 +25,99 @@ var builtins = map[string]*object.Builtin{
 			case *object.String:
 				len := len(arg.Value)
 				return &object.Integer{Value: int64(len)}
+			case *object.Array:
+				len := len(arg.Elements)
+				return &object.Integer{Value: int64(len)}
 			default:
 				return newError("argument to `len` not supported, got %s",
+					args[0].Type())
+			}
+		},
+	},
+	"first": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1",
+					len(args))
+			}
+			switch arg := args[0].(type) {
+			case *object.Array:
+				len := len(arg.Elements)
+				if len == 0 {
+					return NULL
+				}
+				return arg.Elements[0]
+			default:
+				return newError("argument to `first` must be ARRAY, got %s",
+					args[0].Type())
+			}
+		},
+	},
+	"last": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1",
+					len(args))
+			}
+			switch arg := args[0].(type) {
+			case *object.Array:
+				len := len(arg.Elements)
+				if len == 0 {
+					return NULL
+				}
+				return arg.Elements[len-1]
+			default:
+				return newError("argument to `last` must be ARRAY, got %s",
+					args[0].Type())
+			}
+		},
+	},
+	"push": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 2 {
+				return newError("wrong number of arguments. got=%d, want=2",
+					len(args))
+			}
+			switch arr := args[0].(type) {
+			case *object.Array:
+				// Arrays are immutable in monkey
+				length := len(arr.Elements)
+				newElements := make(
+					[]object.Object,
+					length+1,
+				)
+				copy(newElements, arr.Elements)
+				newElements[length] = args[1]
+				return &object.Array{Elements: newElements}
+
+			default:
+				return newError("argument to `push` must be ARRAY, got %s",
+					args[0].Type())
+			}
+		},
+	},
+	"rest": {
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=2",
+					len(args))
+			}
+			switch arr := args[0].(type) {
+			case *object.Array:
+				// Arrays are immutable in monkey
+				length := len(arr.Elements)
+				if length == 0 {
+					return NULL
+				}
+				newElements := make(
+					[]object.Object,
+					length-1,
+				)
+				copy(newElements, arr.Elements[1:])
+				return &object.Array{Elements: newElements}
+
+			default:
+				return newError("argument to `push` must be ARRAY, got %s",
 					args[0].Type())
 			}
 		},
